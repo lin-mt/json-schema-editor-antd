9da849dbd1de3761010fccc7a5bc02892e46635b
lin-mt <lin-mt@outlook.com>
2022-10-10 00:06:18 +0800

    feat: use debounce.

--- src/components/field-input/index.tsx
+++ src/components/field-input/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, ReactNode, useEffect, useRef, useState } from 'react';
 import { Input, InputRef, message } from 'antd';
+import _ from 'lodash';
 
 interface FieldInputProp {
   value: string;
@@ -39,12 +40,15 @@ const FieldInput = (props: FieldInputProp): ReactElement => {
   };
 
   useEffect(() => {
-    ref.current.input.addEventListener('blur', () => {
-      if (ref.current.input.value.length === 0) {
-        message.warn('FieldName can not empty.').then();
-        ref.current.input.focus();
-      }
-    });
+    ref.current.input.addEventListener(
+      'blur',
+      _.debounce(() => {
+        if (ref.current.input.value.length === 0) {
+          message.warn('FieldName can not empty.').then();
+          ref.current.input.focus();
+        }
+      }, 50)
+    );
   }, []);
 
   return (
